6fe0be6409d7b222c51f28ac17247fbc2ab0588e
asledz <anita.gaja.sledz@gmail.com>
2019-01-18 11:01:35 +0100

    Druga wersja programu
    Nie pętli się dla większości testów

--- przelewanka.ml
+++ przelewanka.ml
@@ -1,5 +1,5 @@
-(** PRZELEWANKA **)
-(* Anita Śledź *)
+(** Zadanie przelewanka **)
+(* Anita Śledź 406384 *)
 (* Review - Stanisław Strzelecki *)
 
 
@@ -11,58 +11,56 @@ let przelewanka tab =
 	Queue.add (Array.make n 0) q;
 	Hashtbl.add mapa (Array.make n 0) 0;
 
-	(* my_state - końcowy stan który chcemy otrzymać *)
-	let my_state = (Array.make n 0) in
-	for i = 0 to n-1 do my_state.(i) <- snd (tab.(i)) done;
+	(* Szukany stan *)
+	let final_state = (Array.make n 0) in
+	for i = 0 to n-1 do final_state.(i) <- (snd (tab.(i))) done;
 
 	(* BFS po wszystkich stanach *)
-	while (Queue.is_empty q = false) 
+	while (Queue.is_empty q = false)
 	do
-		(* state - stan aktualnie rozpatrywany z góry kolejki *)
 		let state = Queue.take q in
 		let dis = Hashtbl.find mapa state in
 
-		(* funkcja przelej tworzy tablicę ze stanem po przelaniu wody ze szklanki a do szklanki b *)
-		let przelej a b =
-			let nb = state
-			and poj = fst (tab.(b)) in
-			if (a<>b && state.(a) > 0) then
-			(
-				if (state.(a) + state.(b)) <= poj then ( nb.(a) <- 0; nb.(b) <- (state.(a) + state.(b)); nb )
-				else (nb.(b) <- poj; nb.(a) <- (state.(a) - (poj-state.(b))); nb )
-			)
-			else nb
-
-		(* funkcja uzupełnij tworzy tablicę ze stanem po uzupełnieniu szklanki a do pełna *)
-		and uzupelnij a = 
-			let nb = state
-			and poj = fst (tab.(a)) in
-			nb.(a) <- poj; nb
-		
-		(* funkcja zeruj tworzy tablicę ze stanem po wylaniu wody ze szklanki a*)
-		and zeruj a = 
-			let nb = state in
-			nb.(a) <- 0; nb
-
-		(* funkcja dodaj dodaje nowy stan t do mapy i do kolejki *)
-		and dodaj t = 
-			if Hashtbl.mem mapa t then () else (Hashtbl.add mapa t (dis+1) ; Queue.add t q)
-		in
-
 		for i = 0 to (n-1) do
-			for j = 0 to (n-1) do
-				dodaj (przelej i j)
+			for j = 0 to (n-1) do 
+				let nb = Array.copy state 
+				and cap = fst (tab.(j)) in
+
+				if (i <> j && state.(i) > 0) then (
+					if (state.(i) + state.(j)) <= cap then (
+						nb.(i) <- 0;
+						nb.(j) <- (state.(i) + state.(j) )
+					)
+					else (
+						nb.(i) <- (state.(i) - (cap - state.(j)));
+						nb.(j) <- cap
+					)
+				)
+				else ();
+
+				if Hashtbl.mem mapa nb then ()
+				else (Hashtbl.add mapa nb (dis+1); Queue.add nb q)
+
 			done
 		done;
 
 		for i = 0 to (n-1) do
-			dodaj (uzupelnij i);
-			dodaj (zeruj i)
+			let nb = Array.copy state
+			and nb2 = Array.copy state
+			and cap = fst (tab.(i)) in
+			nb.(i) <- cap;
+			if Hashtbl.mem mapa nb then ()
+			else (Hashtbl.add mapa nb (dis+1); Queue.add nb q);
+			nb2.(i) <- 0;
+			if Hashtbl.mem mapa nb2 then ()
+			else (Hashtbl.add mapa nb2 (dis+1); Queue.add nb2 q)
 		done;
 
+		if state = final_state then Queue.clear q else ();
 
 	done;
 
-	if (Hashtbl.mem mapa my_state) then (Hashtbl.find mapa my_state) else -1
+	if (Hashtbl.mem mapa final_state) then (Hashtbl.find mapa final_state) else -1
+
 
 ;;
